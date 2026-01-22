88656bb9d04d81a772f0be4c407255e3c335ecfc
Md Abdur Rahman Shahed <abrshahed1999@gmail.com>
2026-01-22 11:44:31 +0600

    views

---
+++ view.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
+
+include '../controllers/viewController.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="../views/styles.css">
+</head>
+<body>
+
+<div class="topnav">
+  <a class="active" href="#">Home</a>
+  <a href="LostandFound.php">Lost & Found</a>
+  <a href="AboutUs.php">About Us</a>
+  <a href="update_profile.php">Update Profile</a>
+  <a href="change_password.php">Change Password</a>
+  <a href="../controllers/Logout.php">Logout</a>
+  
+</div>
+
+<div class="homebody">
+    <div class="row">
+        <?php foreach ($posts as $row): ?>
+        <div class="column">
+            <div class="card">
+                <div class="containerbox">
+                    <img src="../views/uploads/<?php echo htmlspecialchars($row['image']); ?>" alt="<?php echo htmlspecialchars($row['title']); ?>" style="width:100%">
+                    <h2 id="cardtitle"><?php echo htmlspecialchars($row['title']); ?></h2>
+                    <p class="title"><?php echo htmlspecialchars(ucfirst($row['status'])); ?></p>
+                    
+                    <?php if (isset($_SESSION['email'])): ?>
+                        <p class="desc">Posted by: <?php echo htmlspecialchars($_SESSION['email']); ?></p> 
+                    <?php endif; ?>
+                    
+                </div>
+            </div>
+        </div>
+        <?php endforeach; ?>
+    </div>
+</div>
+
+<footer>
+    <p>@copyright Md Abdur Rahman Shahed</p>
+</footer>
+</body>
+</html>
